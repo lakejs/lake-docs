e755093747cf301b546a598e8c73b387cd3a157b
luolonghao <luolonghao@gmail.com>
2024-04-28 14:28:54 +0800

    feat: add loading effect for DefaultEdtior

--- .vitepress/theme/DefaultEditor.vue
+++ .vitepress/theme/DefaultEditor.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
 
+const loadingRef = ref(null);
 const editorRef = ref(null);
 const toolbarRef = ref(null);
 const contentRef = ref(null);
@@ -8,6 +9,10 @@ const contentRef = ref(null);
 const props = defineProps(['lang', 'value', 'toolbar', 'rootStyle']);
 
 let editor = null;
+
+const timeoutId = window.setTimeout(() => {
+  loadingRef.value.style.display = 'block';
+}, 1000);
 
 onMounted(() => {
   if (editor) {
@@ -34,7 +39,9 @@ onMounted(() => {
       editor.root.css(props.rootStyle);
     }
     editor.render();
-    editorRef.value.style.visibility = 'visible';
+    window.clearTimeout(timeoutId);
+    loadingRef.value.style.display = 'none';
+    editorRef.value.style.display = 'block';
   })();
 });
 onUnmounted(() => {
@@ -46,13 +53,33 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="vp-raw" ref="editorRef" style="visibility: hidden;">
+  <div :class="$style.loading" ref="loadingRef">
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M232,128a104,104,0,0,1-208,0c0-41,23.81-78.36,60.66-95.27a8,8,0,0,1,6.68,14.54C60.15,61.59,40,93.27,40,128a88,88,0,0,0,176,0c0-34.73-20.15-66.41-51.34-80.73a8,8,0,0,1,6.68-14.54C208.19,49.64,232,87,232,128Z"></path></svg>
+  </div>
+  <div class="vp-raw" ref="editorRef" style="display: none;">
     <div :class="$style.toolbar" ref="toolbarRef"></div>
     <div :class="$style.content" ref="contentRef"></div>
   </div>
 </template>
 
 <style module>
+@keyframes lakeRotate {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+.loading {
+  padding-top: 64px;
+  text-align: center;
+  display: none;
+}
+.loading svg {
+  display: inline;
+  animation: lakeRotate 1s linear 0s infinite normal none;
+}
 .toolbar {
   border: 1px solid #d9d9d9;
   border-top-left-radius: 8px;
